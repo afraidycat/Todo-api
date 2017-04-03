c8644d25f38669ed1e5cec01c6cb2e3051801983
Aaron Pantoja <pantojaaron@gmail.com>
2017-04-03 13:37:49 -0700

    Added DELETE todos route

--- server.js
+++ server.js
@@ -46,6 +46,21 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    var todoDelete = _.without(todos, matchedTodo);
+    todos = todoDelete;
+
+    if (!matchedTodo) {
+        res.status(404).json({"Error": "no todo found with that ID"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT);
