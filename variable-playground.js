1a74a41594a8fa441a64ba631d2cdfa64a771aae
Aaron Pantoja <pantojaaron@gmail.com>
2017-04-03 18:03:27 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+//     name: 'Aaron',
+//     age: 22
+// };
+
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name: 'Aaron',
+//     //     age: 37
+//     // };
+//     obj.age = 37;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var arrayGrades = [82, 79];
+
+function addGrade(arrGrades) {
+    arrGrades.push(92, 88);
+    debugger;
+}
+
+addGrade(arrayGrades);
+console.log(arrayGrades);
